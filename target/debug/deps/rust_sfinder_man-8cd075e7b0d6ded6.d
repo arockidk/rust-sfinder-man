84a81e58b0fd2ab7bbd15258ab7e625e65146cf2
arockidk <joeykicker2018@gmail.com>
2023-10-26 06:36:38 +1100

    add files

---
+++ target/debug/deps/rust_sfinder_man-8cd075e7b0d6ded6.d
@@ -0,0 +1,5 @@
+C:\Users\maril\Documents\dev\chrome-tris\bettersfinder\rust-sfinder-man\target\debug\deps\rust_sfinder_man-8cd075e7b0d6ded6.rmeta: src\lib.rs
+
+C:\Users\maril\Documents\dev\chrome-tris\bettersfinder\rust-sfinder-man\target\debug\deps\rust_sfinder_man-8cd075e7b0d6ded6.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/rust_sfinder_man-da79911c3dd7aa66.d
@@ -0,0 +1,5 @@
+C:\Users\maril\Documents\dev\chrome-tris\bettersfinder\rust-sfinder-man\target\debug\deps\rust_sfinder_man-da79911c3dd7aa66.rmeta: src\lib.rs
+
+C:\Users\maril\Documents\dev\chrome-tris\bettersfinder\rust-sfinder-man\target\debug\deps\rust_sfinder_man-da79911c3dd7aa66.d: src\lib.rs
+
+src\lib.rs:
